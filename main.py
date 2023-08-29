0171ec7b4ce4f807b508d84eb2cf2b6ab6a79a85
Edtrea <86367432+Edtrea@users.noreply.github.com>
2023-08-29 08:08:33 +0000

    Completed part where statement does not end in period and test passed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from typing import Dict, List
 
+
 def GetClass(keyword: str, classes: List[Dict]):
   for dict in classes:
     for key in dict.keys():
@@ -7,11 +8,29 @@ def GetClass(keyword: str, classes: List[Dict]):
         return dict[keyword]
   return None
 
+
 def GetListOfKeysFromDict(dict):
   listOfKeys = []
   for key in dict.keys():
     listOfKeys.append(key)
   return listOfKeys
+
+
+def GetFilteredListOfKeysFromDict(dict, keyword):
+  listOfKeys = []
+  for key in dict.keys():
+    if key.startswith(keyword) == True:
+      listOfKeys.append(key)
+  return listOfKeys
+
+
+def GetFilteredList(list, keyword):
+  listOfKeys = []
+  for value in list:
+    if value.startswith(keyword) == True:
+      listOfKeys.append(value)
+  return listOfKeys
+
 
 def getNextProbableWords(classes: List[Dict],
                          statements: List[str]) -> Dict[str, List[str]]:
@@ -22,24 +41,24 @@ def getNextProbableWords(classes: List[Dict],
   for statement in statements:
     splitStatement = statement.split(".")
     # Check if the statements value ends in period
-    if splitStatement[-1] == "": # statement ends in period
+    if splitStatement[-1] == "":  # statement ends in period
       # if the statements value ends in period, it is a key to search for in classes
       splitStatement.pop()
       if len(splitStatement) == 1:
         # Loop through classes using the key found from statements
-        classObjectValue = GetClass(splitStatement[-1],classes)
-        if classObjectValue is None: # key not found in classes
-          output[splitStatement[-1]] = ""
+        classObjectValue = GetClass(splitStatement[-1], classes)
+        if classObjectValue is None or classObjectValue == "":  # key not found in classes
+          output[statement] = [""]
           continue
-        else: # key found
-          if type(classObjectValue) == dict: 
+        else:  # key found
+          if type(classObjectValue) == dict:
             # If the object found in classes has a Dict for value
             #  Extract the Key from the inner Dict into a list
             innerDictList = GetListOfKeysFromDict(classObjectValue)
             #  Sort the list in alphebatical order
             innerDictList.sort()
             #  add the top 5 from the list as output for the statement
-            output[splitStatement[-1]] = innerDictList[0:5]
+            output[statement] = innerDictList[0:5]
             continue
 
           # check if object is list
@@ -48,25 +67,37 @@ def getNextProbableWords(classes: List[Dict],
             # sort list in alphebatical order
             classObjectValue.sort()
             # add the top 5 from the list for output
-            output[splitStatement[-1]] = classObjectValue[0:5]
+            output[statement] = classObjectValue[0:5]
             continue
       # This is a polymorphic type
       elif len(splitStatement) > 1:
         # returns empty list for output
-        output[splitStatement[-1]] = ""
+        output[statement] = [""]
         continue
-    else:  
+    else:
       # if the statements value does not end in period, it is a parameter to compare with
-      #  Find the word after the second period counting from the right for the key to search for in classes
-      classObjectValue = GetClass(splitStatement[-2],classes)
-      if classObjectValue is None: # key not found in classes
-        output[splitStatement[-2]] = ""
+      if len(splitStatement) == 2:
+        #  Find the word after the second period counting from the right for the key to search for in classes
+        classObjectValue = GetClass(splitStatement[-2], classes)
+        if classObjectValue is None or classObjectValue == "":  # key not found in classes
+          output[statement] = [""]
+          continue
+        if type(classObjectValue) == dict:  #the inner object is a dict
+          innerDictList = GetFilteredListOfKeysFromDict(
+            classObjectValue, splitStatement[-1])
+          innerDictList.sort()
+          output[statement] = innerDictList[0:5]
+          continue
+        if type(classObjectValue) == list:
+          filteredList = GetFilteredList(classObjectValue, splitStatement[-1])
+          filteredList.sort()
+          output[statement] = filteredList[0:5]
+          continue
+      # If len more than 2 it is a polymorphic type
+      elif len(splitStatement) > 2:
+        # returns empty list for output
+        output[statement] = [""]
         continue
-      else:
-        print()
-  
-
-
 
   print(output)
   return output
